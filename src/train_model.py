7235d3f0de948fb80e9774e9860a39905c60464e
Haikal Firdaus <haikalfirdaus498@gmail.com>
2026-03-29 15:24:10 +0700

    pipeline feature engineering, standarization, and encoding

--- src/train_model.py
+++ src/train_model.py
@@ -73,6 +73,66 @@ def standardize_categories(df):
 
     return df
 
+def encode_features(df):
+
+    print("Encoding features...")
+
+    df = df.copy()
+
+    tier_map = {
+        'Iron': 0,
+        'Bronze': 1,
+        'Silver': 2,
+        'Gold': 3,
+        'Platinum': 4
+    }
+
+    df['CRM_PID_Value_Segment'] = df['CRM_PID_Value_Segment'].map(tier_map)
+
+    df = pd.get_dummies(
+        df,
+        columns=['EffectiveSegment'],
+        drop_first=True
+    )
+
+    df['CHURN'] = df['CHURN'].map({
+        'No': 0,
+        'Yes': 1
+    })
+
+    print("Encoding completed")
+
+    return df
+
+def split_feature_target(df):
+
+    print("Splitting feature and target...")
+
+    df = df.copy()
+
+    X = df.drop(columns=['CHURN'])
+    y = df['CHURN']
+
+    print("Split completed")
+
+    return X, y
+
+def split_train_test(X, y):
+
+    print("Creating train test split...")
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+
+    print("Train test split completed")
+
+    return X_train, X_test, y_train, y_test
+
 def main():
 
     df_churn = load_data(PROCESSED_PATH)
